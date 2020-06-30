236e3344b610152d392abfda7a2677934b200984
tingkao <tingkao4124@gmail.com>
2020-06-30 22:10:01 +0800

    week2

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,17 @@
 function join(arr, concatStr) {
-  
+  let result = arr[0]
+  for (var i = 1; i < arr.length; i++) {
+    result = result + concatStr + arr[i]
+  }
+  return result
 }
 
 function repeat(str, times) {
-  
+  let result = ''
+  for (var j = 0; j < times; j++){
+    result = result + str
+  }
+  return result
 }
 
 console.log(join(['a'], '!'));
