7c029ec8f99c8615a92a85ecfc65135d871db4c1
tingkao <tingkao4124@gmail.com>
2020-07-19 12:28:31 +0800

    week4 hw1, fix: use parameter from API

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,9 +1,9 @@
 const request = require('request');
 
-request('https://lidemy-book-store.herokuapp.com/books',
+request('https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
     const bookList = JSON.parse(body);
-    for (let i = 1; i <= 10; i += 1) {
-      console.log(`${bookList[i - 1].id} ${bookList[i - 1].name}`);
+    for (let i = 0; i < bookList.length; i += 1) {
+      console.log(`${bookList[i].id} ${bookList[i].name}`);
     }
   });
