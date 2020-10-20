85eea54a1e7e154a37521f55ca3317e0e68d4ae1
tingkao <tingkao4124@gmail.com>
2020-10-20 19:27:01 +0800

    week12

--- homeworks/week12/hw1/api_index.php
+++ homeworks/week12/hw1/api_index.php
@@ -1,23 +1,59 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-  <script src="main.js"></script>
-</head>
-<body>
-  <h1>標題</h1>
+
 <?php
+  require_once('conn.php');
+  header('Content-type:application/json;charset=utf-8');
+  header('Access-Control-Allow-Origin: *');
+  function sum() {
+    global $conn;
+    $sql_sum = "SELECT COUNT(*) as sum FROM `tingkao_comments` WHERE `is_deleted` = 0 AND `is_hidded` = 0 ORDER BY `created_at` DESC";
+    $stmt_sum = $conn->prepare($sql_sum);
+    $stmt_sum->execute();
+    if($conn->error || $stmt_sum->error){
+      die("錯誤訊息： " . $conn->error . $stmt_sum->error);
+    }
+    $result = $stmt_sum->get_result();
+    return  $result->fetch_assoc()['sum'];
+  };
+
+  $sum = sum();
+  $state = 1;
+  $all_state = ceil($sum / 5);
+  $has_state = true;
+  
+  if(!empty($_GET['state'])){
+    $state = intval($_GET['state']);
+    if($state > 0 && $state <= $all_state){
+      $offset = ($state - 1) * 5;
+      $sql = "SELECT * FROM `tingkao_comments` WHERE `is_deleted` = 0 AND `is_hidded` = 0 ORDER BY `created_at` DESC LIMIT 5 OFFSET ?";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("i", $offset);
+    } 
+    if($state == $all_state){
+      $has_state = false;
+    }
+  }else {
+    $sql = "SELECT * FROM `tingkao_comments` WHERE `is_deleted` = 0 AND `is_hidded` = 0 ORDER BY `created_at` DESC LIMIT 5 ";
+    $stmt = $conn->prepare($sql);
+  }
+  $stmt->execute();
+  if($conn->error || $stmt->error){
+    die("錯誤訊息： " . $conn->error . $stmt->error);
+  }
+  $result = $stmt->get_result();
   $comments = array();
-  array_push($comments, array(
-    "id" => "小明",
-    "username" => "小明",
-    "nickname" => "還是小明",
-  ));
-  echo 123;
+  while($row = $result->fetch_assoc()) {
+    array_push($comments, array(
+      "username" => $row['username'],
+      "content" => $row['content'],
+      "created_at" => $row['created_at'],
+    ));
+  }
+  $json = array(
+    "comments" => $comments,
+    "state" => $has_state,
+  );
+
+  $response = json_encode($json);
+  echo $response;
 ?>
-</body>
-</html>
+
